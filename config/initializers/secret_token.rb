5b24a6886052485c107f9b84964c4dab5b1dcb79
Arpit Bajpai <bajpai.arpit.ab1992@gmail.com>
2014-08-20 10:11:05 +0000

    Initializing repository for learning One month rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = 'f7ea97bae1cee03d4097b749decbc3317594059656891c238eeead4b47177182e11168b4633a1be215f695a283e460aa6fd1da5c3de04f7a2d0edb284934c228'
