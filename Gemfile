45aca9d4f0cd780c6a513754dc0e24ddf9d7ff0b
Arpit Bajpai <bajpai.arpit.ab1992@gmail.com>
2014-08-22 10:25:05 +0000

    added devise user authentication

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+ruby '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
@@ -26,6 +26,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass'
+
+gem 'devise'
 
 group :test, :development do
 	gem 'sqlite3'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Pinteresting::Application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
+  config.action_mailer.default_url_options = { :host => 'http://arpit-learning.herokuapp.com/' }
   # Disable automatic flushing of the log to improve performance.
   # config.autoflush_log = false
 
